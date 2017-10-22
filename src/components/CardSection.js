db11c178922f6962d5e83493ba9d24b26498e4ce
aofleejay <aofleejay@hotmail.com>
2017-10-22 14:17:14 +0700

    music album ui

---
+++ src/components/CardSection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const CardSection = props => (
+  <View style={styles.container}>
+    {props.children}
+  </View>
+)
+
+const styles = {
+  container: {
+    padding: 5,
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+  }
+}
+
+export default CardSection

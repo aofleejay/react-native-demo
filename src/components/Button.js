2c9b9440fc184bf0ed4614bf097f31f23fa6ae6a
aofleejay <aofleejay@hotmail.com>
2017-10-22 16:03:40 +0700

    button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,29 @@ import React from 'react'
 import { TouchableOpacity, Text } from 'react-native'
 
 const Button = () => (
-  <TouchableOpacity>
-    <Text>Buy now</Text>
+  <TouchableOpacity style={styles.button}>
+    <Text style={styles.text}>Buy now</Text>
   </TouchableOpacity>
 )
+
+const styles = {
+  text: {
+    alignSelf: 'center',
+    color: '#007AFF',
+    fontSize: 16,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    marginLeft: 5,
+    marginRight: 5,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#007AFF',
+  },
+}
 
 export default Button
